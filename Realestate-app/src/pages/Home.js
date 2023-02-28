d0e0c754624dfb02a02410813a6dd29ec5599dc3
Emeka Agara <66171600+EmekaAgara@users.noreply.github.com>
2023-02-28 13:33:11 +0100

    locations added

--- Realestate-app/src/pages/Home.js
+++ Realestate-app/src/pages/Home.js
@@ -3,7 +3,7 @@ import "./Home.css";
 import { Link } from "react-router-dom";
 import bg from "../images/homeBG.jpg"
 import logo from "../images/rentr.png"
-import {ConnectButton} from "web3uikit"
+import {ConnectButton, Select} from "web3uikit"
 
 const Home = () => {
 
@@ -31,18 +31,47 @@ const Home = () => {
       <div className="tabContent">
         <div className="searchFields">
         <div className="inputs">
+          Location
+          <Select
+              defaultOptionIndex={0}
+              onChange={(data)  =>  console.log(data.label)}
+              options={[
+                {
+                  id:"lg",
+                  label:"Lagos, Nigeria"
+                },
+
+                {
+                  id:"lg",
+                  label:"Victoria Island"
+                },
+
+                {
+                  id:"lg",
+                  label:"Lekki, Lagos"
+                },
+
+                {
+                  id:"lg",
+                  label:"Abuja, Nigeria"
+                },
+              ]}
+          />
 
         </div>
         <div className="vl"/>
         <div className="inputs">
+          Check In
 
         </div>
         <div className="vl"/>
         <div className="inputs">
+          Check Out
 
         </div>
         <div className="vl"/>
         <div className="inputs">
+          Guests
         
         </div>
       </div>
